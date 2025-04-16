620a154008fa6228ceb855dd12f2cecb0545290e
stepanpigriteus <avetor-sport@yandex.ru>
2025-04-16 13:02:39 +0500

    add config, logger

--- 1337b04rd/cmd/1337b04rd/main.go
+++ 1337b04rd/cmd/1337b04rd/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"1337b04rd/pkg/flags"
+	"1337b04rd/pkg/logger"
 )
 
-
 func main() {
-	flags.Flags()
+	port := flags.Flags()
+
+	logger := logger.NewSlogAdapter()
+	logger.Info("User created", "user_id", port, "email", "rick@example.com")
 }
