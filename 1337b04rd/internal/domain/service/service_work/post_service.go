ed21be110584b40c36da9c44744a008cc3c13c23
vstepano <student@ALEM-F4-R03-02.alem.school>
2025-04-18 14:31:32 +0500

    start service

--- 1337b04rd/internal/domain/service/service_work/post_service.go
+++ 1337b04rd/internal/domain/service/service_work/post_service.go
@@ -1,3 +1,34 @@
 package servicework
 
+import (
+	"1337b04rd/internal/domain/entity"
+	"1337b04rd/internal/domain/port/repository"
+	"context"
+)
 
+type postService struct {
+	postRepo repository.PostRepository
+}
+
+func NewPostService(postRepo repository.PostRepository) *postService {
+	return &postService{
+		postRepo: postRepo,
+	}
+}
+
+func CreatePost(ctx context.Context, post entity.Post) (entity.Post, error) {
+	return post, nil
+}
+
+func GetPostByID(ctx context.Context, id int) (entity.Post, error) {
+	var post entity.Post
+	return post, nil
+}
+
+func UpdatePost(ctx context.Context, id int, post entity.Post) (entity.Post, error) {
+	return post, nil
+}
+
+func DeletePost(ctx context.Context, id int) error {
+	return nil
+}
